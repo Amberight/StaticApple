c1732a4eca85e7aadf216dcccee37084e44d2294
Amberight <41237397+Amberight@users.noreply.github.com>
2018-07-14 21:38:57 +0300

    Added another static
    Study on static variables, inheritance and abstract classes

--- StaticApple/Apples.cpp
+++ StaticApple/Apples.cpp
@@ -3,17 +3,19 @@
 #include <iostream>
 
 static int s_Amount = 0;
+static int s_sumWeight = 0;
 
 Apples::Apples(int weight, std::string colour)
 {
 	s_Amount++;
 	this->weight = weight;
 	this->colour = colour;
+	s_sumWeight = s_sumWeight + weight;
 }
 
 void Apples::Print()
 {
-	printf("There are %d %s %s %d %s", s_Amount, colour.c_str(), "apples, weighing", weight, "gramms.\n");
+	printf("There are %d %s %s %d %s", s_Amount, colour.c_str(), "apples, weighing", s_sumWeight, "gramms.\n");
 }
 
 Apples::~Apples()
--- StaticApple/Pear.cpp
+++ StaticApple/Pear.cpp
@@ -3,17 +3,19 @@
 #include <iostream>
 
 static int s_Amount = 0;
+static int s_sumWeight = 0;
 
 Pear::Pear(int weight, std::string colour)
 {
 	s_Amount++;
 	this->weight = weight;
 	this->colour = colour;
+	s_sumWeight = s_sumWeight + weight;
 }
 
 void Pear::Print()
 {
-	printf("There are %d %s %s %d %s", s_Amount, colour.c_str(), "pears, weighing", weight, "gramms.\n");
+	printf("There are %d %s %s %d %s", s_Amount, colour.c_str(), "pears, weighing", s_sumWeight, "gramms.\n");
 }
 
 Pear::~Pear()
